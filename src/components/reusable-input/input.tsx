cdb7b9bb1baa05b1baabe4aded89da2ca21f8b6c
Paxlavon2206 <paxlavon2206@gmail.com>
2024-04-23 08:35:06 +0500

    daars

---
+++ src/components/reusable-input/input.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+interface InputProps {
+    text?: string
+    className?: string
+    type?: 'text' | 'password' | 'number'
+    placeholder?: string
+    value?: string
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void}
+
+
+export const Input = ({text, className, type, placeholder, value, onChange}: InputProps) => {
+  return (
+    <input className={className} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+  )
+}
